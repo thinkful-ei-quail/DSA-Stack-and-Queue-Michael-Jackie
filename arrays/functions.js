64424ca8cf6ce91773b296ebb5aebf3b41a76559
Jackie Abert <jax.abert@gmail.com>
2020-09-23 15:03:28 -0400

    commit

--- arrays/functions.js
+++ arrays/functions.js
@@ -2,15 +2,15 @@ const Array = require('./array');
 
 function main(){
     Array.SIZE_RATIO = 3;
-    // Create an instance of the Array class
     let arr = new Array();
-    // Add an item to the array
-    arr.push('testing');
+    arr.push(22);
     arr.push(5);
     arr.push(15);
     arr.push(19);
     arr.push(45);
     arr.push(10);
+    console.log(arr.get(2))
     console.log(arr)
 }
 main()
+
