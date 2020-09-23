316398ad4e38c98e3957763ac3b49158639bd986
Jackie Abert <jax.abert@gmail.com>
2020-09-23 12:32:48 -0400

    commit

--- arrays/functions.js
+++ arrays/functions.js
@@ -10,11 +10,11 @@ function main(){
 
     // Add an item to the array
     arr.push(3);
-    arr.push(5);
-    arr.push(15);
-    arr.push(19);
-    arr.push(45);
-    arr.push(10);
+    // arr.push(5);
+    // arr.push(15);
+    // arr.push(19);
+    // arr.push(45);
+    // arr.push(10);
 
     console.log(arr);
 }
