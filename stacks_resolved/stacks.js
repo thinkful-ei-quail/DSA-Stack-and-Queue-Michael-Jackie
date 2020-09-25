1cc0b648a42a1be99c2a16cc29a61e07f43d513c
Michael Oldacre <mloldacre@live.com>
2020-09-25 11:44:38 -0400

    Fixed indent issue

--- stacks_resolved/stacks.js
+++ stacks_resolved/stacks.js
@@ -1,41 +1,42 @@
+/* eslint-disable indent */
 class _Node {
-  constructor(data, next) {
-    this.data = data;
-    this.next = next;
-  }
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
 class Stack {
-  constructor() {
-    this.top = null;
-  }
-  push(data) {
-    if (this.top === null) {
-      this.top = new _Node(data, null);
-      return this.top;
+    constructor() {
+        this.top = null;
     }
-    const node = new _Node(data, this.top);
-    this.top = node;
-  }
-  pop() {
-    const node = this.top;
-    this.top = node.next;
-    return node.data;
-  }
+    push(data) {
+        if (this.top === null) {
+            this.top = new _Node(data, null);
+            return this.top;
+        }
+        const node = new _Node(data, this.top);
+        this.top = node;
+    }
+    pop() {
+        const node = this.top;
+        this.top = node.next;
+        return node.data;
+    }
 }
 
 //#1
 function main() {
-  const starTrek = new Stack;
-  starTrek.push('Kirk');
-  starTrek.push('Spock');
-  starTrek.push('McCoy');
-  starTrek.push('Scotty');
-  starTrek.pop();
-  starTrek.pop();
-  starTrek.pop();
-  // starTrek.pop('McCoy')
-  return starTrek;
+    const starTrek = new Stack;
+    starTrek.push('Kirk');
+    starTrek.push('Spock');
+    starTrek.push('McCoy');
+    starTrek.push('Scotty');
+    starTrek.pop();
+    starTrek.pop();
+    starTrek.pop();
+    // starTrek.pop('McCoy')
+    return starTrek;
 }
 console.log(main());
 
@@ -45,54 +46,54 @@ console.log('STACK', stack);
 
 //#2
 function peek(stack) {
-  if (stack.top === null) {
-    return 'stack is empty';
-  }
-  return stack.top.data;
+    if (stack.top === null) {
+        return 'stack is empty';
+    }
+    return stack.top.data;
 }
 console.log('peek', peek(stack));
 
 function isEmpty(stack) {
-  if (stack.top === null) {
-    return 'Stack is empty';
-  } else {
+    if (stack.top === null) {
+        return 'Stack is empty';
+    } else {
 
-    if (stack.top !== null) {
-      return 'items in stack';
+        if (stack.top !== null) {
+            return 'items in stack';
+        }
     }
-  }
 }
 
 console.log(isEmpty(stack));
 
 function display(stack) {
-  if (stack.top === null) {
-    return 'stack is empty';
-  }
-  let node = stack.top;
-  let string = '';
-  while (node !== null) {
-    string += node.data;
-    if (node.next !== null) string += '->';
-    node = node.next;
-  }
-  return string;
+    if (stack.top === null) {
+        return 'stack is empty';
+    }
+    let node = stack.top;
+    let string = '';
+    while (node !== null) {
+        string += node.data;
+        if (node.next !== null) string += '->';
+        node = node.next;
+    }
+    return string;
 }
 
 console.log('display', display(stack));
 
 //#3
-function is_palindrome(s) {
-  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-  let reverse = new Stack();
-  for (let i = 0; i < s.length; i++) {
-    reverse.push(s.charAt(i));
-  }
-  for (let j = 0; j < s.length; j++) {
-    if (s[j] === reverse.pop()) continue;
-    else return false;
-  }
-  return true;
+function is_palindrome(str) {
+    str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reverse = new Stack();
+    for (let i = 0; i < str.length; i++) {
+        reverse.push(str.charAt(i));
+    }
+    for (let j = 0; j < str.length; j++) {
+        if (str[j] === reverse.pop()) continue;
+        else return false;
+    }
+    return true;
 }
 console.log(is_palindrome('dad'));
 console.log(is_palindrome('A man, a plan, a canal: Panama'));
