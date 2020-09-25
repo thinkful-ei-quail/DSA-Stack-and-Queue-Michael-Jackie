eb397bca3458b99ce7cd5d2282d5281f45784267
Jackie Abert <jax.abert@gmail.com>
2020-09-25 14:06:05 -0400

    parens

--- stacks_resolved/stacks.js
+++ stacks_resolved/stacks.js
@@ -1,126 +1,124 @@
 /* eslint-disable indent */
 class _Node {
-    constructor(data, next) {
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class Stack {
-    constructor() {
-        this.top = null;
+  constructor() {
+    this.top = null;
+  }
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
     }
-    push(data) {
-        if (this.top === null) {
-            this.top = new _Node(data, null);
-            return this.top;
-        }
-        const node = new _Node(data, this.top);
-        this.top = node;
-    }
-    pop() {
-        const node = this.top;
-        this.top = node.next;
-        return node.data;
-    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
 }
 
 //#1
 function main() {
-    const starTrek = new Stack;
-    starTrek.push('Kirk');
-    starTrek.push('Spock');
-    starTrek.push('McCoy');
-    starTrek.push('Scotty');
-    starTrek.pop();
-    starTrek.pop();
-    starTrek.pop();
-    // starTrek.pop('McCoy')
-    return starTrek;
+  const starTrek = new Stack();
+  starTrek.push("Kirk");
+  starTrek.push("Spock");
+  starTrek.push("McCoy");
+  starTrek.push("Scotty");
+  starTrek.pop();
+  starTrek.pop();
+  starTrek.pop();
+  // starTrek.pop('McCoy')
+  return starTrek;
 }
 console.log(main());
 
 const stack = main();
-console.log('STACK', stack);
-
+console.log("STACK", stack);
 
 //#2
 function peek(stack) {
-    if (stack.top === null) {
-        return 'stack is empty';
-    }
-    return stack.top.data;
+  if (stack.top === null) {
+    return "stack is empty";
+  }
+  return stack.top.data;
 }
-console.log('peek', peek(stack));
+console.log("peek", peek(stack));
 
 function isEmpty(stack) {
-    if (stack.top === null) {
-        return 'Stack is empty';
-    } else {
-
-        if (stack.top !== null) {
-            return 'items in stack';
-        }
+  if (stack.top === null) {
+    return "Stack is empty";
+  } else {
+    if (stack.top !== null) {
+      return "items in stack";
     }
+  }
 }
 
 console.log(isEmpty(stack));
 
 function display(stack) {
-    if (stack.top === null) {
-        return 'stack is empty';
-    }
-    let node = stack.top;
-    let string = '';
-    while (node !== null) {
-        string += node.data;
-        if (node.next !== null) string += '->';
-        node = node.next;
-    }
-    return string;
+  if (stack.top === null) {
+    return "stack is empty";
+  }
+  let node = stack.top;
+  let string = "";
+  while (node !== null) {
+    string += node.data;
+    if (node.next !== null) string += "->";
+    node = node.next;
+  }
+  return string;
 }
 
-console.log('display', display(stack));
+console.log("display", display(stack));
 
 //#3
 function isPalindrome(str) {
-    str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    let reverse = new Stack();
-    for (let i = 0; i < str.length; i++) {
-        reverse.push(str.charAt(i));
-    }
-    for (let j = 0; j < str.length; j++) {
-        if (str[j] === reverse.pop()) continue;
-        else return false;
-    }
-    return true;
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let reverse = new Stack();
+  for (let i = 0; i < str.length; i++) {
+    reverse.push(str.charAt(i));
+  }
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] === reverse.pop()) continue;
+    else return false;
+  }
+  return true;
 }
-console.log(isPalindrome('dad'));
-console.log(isPalindrome('A man, a plan, a canal: Panama'));
-console.log(isPalindrome('1001'));
-console.log(isPalindrome('Tauhida'));
+console.log(isPalindrome("dad"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("1001"));
+console.log(isPalindrome("Tauhida"));
 
 //#4
 function parenthesesCheck(str) {
-    const strStack = new Stack();
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === '(') {
-            strStack.push(str[i]);
-            console.log('1st IF',strStack);
-        }
-        if( str[i] === ')'){
-           // if(peek(strStack) === '(' && str[i] === ')')
-                strStack.pop();
-            console.log('2nd IF', strStack);
-        }
+  const strStack = new Stack();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      strStack.push(str[i]);
     }
-    console.log('COMPLETE', strStack);
-    
-    if(strStack.length === 0){
-        return true;
-    } else {
-        return false;
+    if(str[i] === ")" && strStack.top === null) {
+      strStack.push(str[i]);
     }
+    if (str[i] === ")" && strStack.top.data === "(") {
+        strStack.pop();
+    }
+  }
+  if (strStack.top === null) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log('1st Pcheck should be true:',parenthesesCheck('()'));
-//console.log('2nd Pcheck should be false:',parenthesesCheck('(('));
+console.log("1st Pcheck should be true:", parenthesesCheck('()()()()()()()()'));
+console.log("2nd Pcheck should be false:", parenthesesCheck("(("));
+console.log("3rd Pcheck should be false:", parenthesesCheck("))"));
+console.log("4th Pcheck should be false:", parenthesesCheck(")"));
